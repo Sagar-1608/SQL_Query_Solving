15ccc007a9a9215c4d33fc2f2192d94a834b8b20
SAGAR JADHAV <113523966+Sagar-1608@users.noreply.github.com>
2023-02-16 00:03:52 +0530

    Create 18_197_Rising Temperature.sql

---
+++ 18_197_Rising Temperature.sql
@@ -0,0 +1,11 @@
+#197. Rising Temperature
+
+# Write your MySQL query statement below
+
+
+SELECT DISTINCT a.Id
+FROM Weather a, Weather b
+WHERE a.Temperature > b.Temperature
+AND DATEDIFF( a.Recorddate, b.Recorddate) = 1
+
+
